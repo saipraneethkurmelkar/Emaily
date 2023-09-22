f5bfe6e40d031d3c7a05ecdd92eb26b922f56bd2
Sai Praneeth Kurmelkar <saipraneethkurmelkar@endpoint-10-192-202-67.ufvisitor.ufl.edu>
2023-09-22 12:48:19 -0400

    proxy removed

--- services/passport.js
+++ services/passport.js
@@ -23,7 +23,7 @@ passport.use(new GoogleStrategy({
     clientID:keys.googleClientID,
     clientSecret:keys.googleClientSecret,
     callbackURL:'/auth/google/callback',
-    proxy: true
+    // proxy: true
 },
 (accessToken,refreshToken,profile,done) =>{
 
